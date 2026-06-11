70c35b69d74dc397fb349b6b4534e94b6c4ae508
qian-anlove <3145946472@qq.com>
2026-06-11 16:16:16 +0800

    优化

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const path = require('path');
+const express = require("express");
+const serverless = require("serverless-http");
+const path = require("path");
 
 // 导入你原有的 Express 应用
 // 假设你的 Express 入口是 server/index.js，且导出了 app 对象
 // 如果你的 server/index.js 没有导出 app，需要稍微改造一下
 // const app = require('../server/index').app || require('../server/index');
-import serverless from 'serverless-http';
-import app from '../server/index.js';  // 必须加 .js 扩展名
+import serverless from "serverless-http";
+import app from "../server/index.js"; // 必须加 .js 扩展名
 
 export const handler = serverless(app);
+
+router.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
